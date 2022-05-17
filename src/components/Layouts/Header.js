33b5f08f0f311a729803440c551e295f2ad21ec8
susantokun <susantokun.com@gmail.com>
2022-05-18 02:41:18 +0700

    Update color for dark mode

--- src/components/Layouts/Header.js
+++ src/components/Layouts/Header.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, useColorScheme } from 'react-native';
 import tw from 'twrnc';
 
 export default function Header(props) {
+  const isDarkMode = useColorScheme() === 'dark';
   const { title } = props;
+
   return (
-    <View style={tw`bg-red-400 h-24 flex w-full items-center justify-center`}>
-      <Text style={tw`text-gray-800 font-bold`}>{title || 'React Native App'}</Text>
+    <View style={tw`${isDarkMode ? 'bg-red-400' : 'bg-red-400'} h-24 flex w-full items-center justify-center`}>
+      <Text style={tw`${isDarkMode ? 'text-black' : 'text-white'} font-bold`}>{title || 'React Native App'}</Text>
     </View>
   );
 }
