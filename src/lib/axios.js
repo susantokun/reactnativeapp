8f6a7a1bc17b43ba46f4a0164e47f76dfcca1d9f
susantokun <susantokun.com@gmail.com>
2022-05-18 02:40:25 +0700

    Add config axios

---
+++ src/lib/axios.js
@@ -0,0 +1,17 @@
+import Axios from 'axios';
+import envs from './env';
+
+export const http = Axios.create({
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
+});
+
+export const axios = Axios.create({
+  baseURL: envs.API_URL,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
+});
