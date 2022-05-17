8fbfb7d0c725ad64dffcd1ab487bd437d58fe7e1
susantokun <susantokun.com@gmail.com>
2022-05-17 22:32:50 +0700

    Fix indent

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,10 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
-    [
-      'babel-plugin-root-import',
-      {
-        rootPathSuffix: './src',
-        rootPathPrefix: '@/',
-      },
-    ],
+    ['babel-plugin-root-import', {
+      rootPathSuffix: './src',
+      rootPathPrefix: '@/',
+    }],
     ['module:react-native-dotenv', {
       moduleName: '@env',
       path: '.env',
